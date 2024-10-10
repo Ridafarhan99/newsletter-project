13e938b463c27ce8aef9fa86cb0635eb2052b105
Rida Farhan <42339599+Ridafarhan99@users.noreply.github.com>
2024-10-10 23:48:56 +0530

    included projectlombok & ResponseEntity

--- src/main/java/com/spring_project/spring_project/controller/NewsletterController.java
+++ src/main/java/com/spring_project/spring_project/controller/NewsletterController.java
@@ -4,13 +4,12 @@ import com.spring_project.spring_project.entity.NewsletterEntry;
 import com.spring_project.spring_project.service.NewsletterEntryService;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 @RequestMapping("/news")
@@ -22,42 +21,55 @@ public class NewsletterController {
     private Map<Long, NewsletterEntry> newsletterEntries= new HashMap<>();
 
     @GetMapping
-    public List<NewsletterEntry> getAll(){
-        return newsletterEntryService.getAllNewsletters();
+    public ResponseEntity<?> getAll(){
+        List<NewsletterEntry> allData = newsletterEntryService.getAllNewsletters();
+        if (allData != null && !allData.isEmpty()){
+            return new ResponseEntity<>(allData, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(allData, HttpStatus.NOT_FOUND);
     }
 
 
     @PostMapping
-    public NewsletterEntry createEntry(@RequestBody NewsletterEntry myEntry){
-        myEntry.setDate(LocalDateTime.now());
-        newsletterEntryService.saveEntry(myEntry);
-        return myEntry;
+    public ResponseEntity<NewsletterEntry> createEntry(@RequestBody NewsletterEntry myEntry){
+        try {
+            myEntry.setDate(LocalDateTime.now());
+            newsletterEntryService.saveEntry(myEntry);
+            return new ResponseEntity<>(myEntry, HttpStatus.CREATED);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
 
     @GetMapping("id/{myID}")
-    public NewsletterEntry getNewsletterEntryById(@PathVariable ObjectId myID){
-        return newsletterEntryService.findById(myID).orElse( null);
+    public ResponseEntity<NewsletterEntry> getNewsletterEntryById(@PathVariable ObjectId myID){
+        Optional<NewsletterEntry> newsletterEntry = newsletterEntryService.findById(myID);
+        if(newsletterEntry.isPresent()){
+            return new ResponseEntity<>(newsletterEntry.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>( HttpStatus.NOT_FOUND);
     }
 
 
     @PutMapping("id/{myID}")
-    public NewsletterEntry updateNewsletterEntryById(@PathVariable ObjectId myID, @RequestBody NewsletterEntry newEntry){
+    public ResponseEntity<?> updateNewsletterEntryById(@PathVariable ObjectId myID, @RequestBody NewsletterEntry newEntry){
         NewsletterEntry oldEntry = newsletterEntryService.findById(myID).orElse(null);
         if(oldEntry != null){
-            oldEntry.setContent(newEntry.getTitle() != null && !newEntry.getTitle().equals("") ? newEntry.getTitle() : oldEntry.getTitle());
+            oldEntry.setTitle(newEntry.getTitle() != null && !newEntry.getTitle().equals("") ? newEntry.getTitle() : oldEntry.getTitle());
             oldEntry.setContent(newEntry.getContent() != null && !newEntry.getContent().equals("") ? newEntry.getContent() : oldEntry.getContent());
+            newsletterEntryService.saveEntry(oldEntry);
+            return new ResponseEntity<>(oldEntry,HttpStatus.OK);
         }
 
-        newsletterEntryService.saveEntry(newEntry);
-        return newEntry;
+        return new ResponseEntity<>(oldEntry,HttpStatus.NOT_FOUND);
     }
 
 
     @DeleteMapping("id/{myID}")
-    public boolean deleteNewsletterEntryById(@PathVariable ObjectId myID){
+    public ResponseEntity<?> deleteNewsletterEntryById(@PathVariable ObjectId myID){
         newsletterEntryService.deleteById(myID);
-        return true;
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 
     }
 }
--- src/main/java/com/spring_project/spring_project/entity/NewsletterEntry.java
+++ src/main/java/com/spring_project/spring_project/entity/NewsletterEntry.java
@@ -1,5 +1,6 @@
 package com.spring_project.spring_project.entity;
 
+import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -8,6 +9,7 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 @Document(collection = "news_entries")
+@Data
 public class NewsletterEntry {
     @Id
     private ObjectId id;
@@ -15,35 +17,35 @@ public class NewsletterEntry {
     private String content;
     private LocalDateTime date;
 
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
+//    public LocalDateTime getDate() {
+//        return date;
+//    }
+//
+//    public void setDate(LocalDateTime date) {
+//        this.date = date;
+//    }
+//
+//    public ObjectId getId() {
+//        return id;
+//    }
+//
+//    public void setId(ObjectId id) {
+//        this.id = id;
+//    }
+//
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public void setTitle(String title) {
+//        this.title = title;
+//    }
+//
+//    public String getContent() {
+//        return content;
+//    }
+//
+//    public void setContent(String content) {
+//        this.content = content;
+//    }
 }
